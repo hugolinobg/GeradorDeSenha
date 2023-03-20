e76758ca1102b2517c4e664c1ebdee032bcc4a7c
Hugolino B. Guimarães <hugolino@hotmail.com.br>
2023-03-20 16:03:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,45 @@
-let sliderElement = document.querySelector("#slider");
-let buttonElement = document.querySelector("#button");
+let sliderElement = document.querySelector("#slider")
+let buttonElement = document.querySelector("#button")
 
-let sizePassword = document.querySelector("#valor");
-let password = document.querySelector("#password");
+let sizePassword = document.querySelector("#valor")
+let password = document.querySelector("#password")
 
-let containerPassword = document.querySelector("#container-password");
+const currentYear = document.querySelector(".newYear")
+
+let containerPassword = document.querySelector("#container-password")
 
 let charset =
-  "abcdefghijklmnopqrstuvwxyz0123456789!@#-$%§£¢*&()_+}}{][?/><ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let novaSenha = "";
+  "abcdefghijklmnopqrstuvwxyz0123456789!@#-$%§£¢*&()_+}}{][?/><ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let novaSenha = ""
 
-sizePassword.innerHTML = sliderElement.value;
+sizePassword.innerHTML = sliderElement.value
 
 slider.oninput = function () {
-  sizePassword.innerHTML = this.value;
-};
+  sizePassword.innerHTML = this.value
+}
 
 function generatePassword() {
-  let pass = "";
+  let pass = ""
 
   for (let i = 0, n = charset.length; i < sliderElement.value; i++) {
-    pass += charset.charAt(Math.floor(Math.random() * n));
+    pass += charset.charAt(Math.floor(Math.random() * n))
   }
-  containerPassword.classList.remove("hide");
-  password.innerHTML = pass;
-  novaSenha = pass;
+  containerPassword.classList.remove("hide")
+  password.innerHTML = pass
+  novaSenha = pass
 }
 
-function copyPassword(){
+async function copyPassword() {
+  try {
+    await navigator.clipboard.writeText(novaSenha)
     alert("Senha copiada com sucesso!")
-    navigator.clipboard.writeText(novaSenha)
+  } catch (err) {
+    console.error("Failed to copy: ", err)
+  }
 }
 
-
+function currentFullYear() {
+  const newYear = new Date().getFullYear()
+  currentYear.textContent = newYear
+}
+currentFullYear()
